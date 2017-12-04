4fbe354b3efb015a03b7cbc3f6e6c8fe7a667a75
Cory Roloff <cory@brothersroloff.com>
2017-12-03 18:53:05 -0600

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
 	extends: 'eslint:recommended',
 	parserOptions: {
 		ecmaVersion: 6,
-		ecmaFeatures: { experimentalObjectRestSpread: true }
+		ecmaFeatures: { experimentalObjectRestSpread: true },
+		sourceType: 'module'
 	},
 	rules: {
 		indent: [
